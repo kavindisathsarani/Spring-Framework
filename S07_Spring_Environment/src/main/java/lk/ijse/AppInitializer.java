fd3aeb288e690a7465742fea7a093fc1e8f0fc57
kavindisathsarani <dksmanimendra0@gmail.com>
2025-01-29 13:48:49 +0530

    module 7 done

--- S07_Spring_Environment/src/main/java/lk/ijse/AppInitializer.java
+++ S07_Spring_Environment/src/main/java/lk/ijse/AppInitializer.java
@@ -4,14 +4,30 @@ package lk.ijse;
 import lk.ijse.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+import java.util.Properties;
+
 public class AppInitializer {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
-
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
-
         context.refresh();
+
+        //System Variables
+        //This gives computer details like OS
+         /*Map<String,String> getenv=System.getenv();
+         for(String key: getenv.keySet()){
+             System.out.println(key+"="+getenv.get(key));
+         }*/
+
+        //Java Predefined Properties
+      /*  Properties properties=System.getProperties();
+        for (String key: properties.stringPropertyNames()){
+            System.out.println(key+"="+properties.getProperty(key));
+        }*/
+
+       /* String osName=System.getProperty("os.name");
+        System.out.println(osName); //Windows 11*/
 
         context.registerShutdownHook();
 
