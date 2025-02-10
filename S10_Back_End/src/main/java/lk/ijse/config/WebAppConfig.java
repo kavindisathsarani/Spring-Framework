1a241fa6ca828d25c442f504403972c7a714e14b
kavindisathsarani <dksmanimendra0@gmail.com>
2025-02-10 08:09:16 +0530

    module 11 done

---
+++ S10_Back_End/src/main/java/lk/ijse/config/WebAppConfig.java
@@ -0,0 +1,13 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = {"lk.ijse.contoller"})
+@EnableWebMvc
+public class WebAppConfig implements WebMvcConfigurer {
+
+}
