1ae7436ab40576af9a92e07b062e1799f22b342a
kavindisathsarani <dksmanimendra0@gmail.com>
2025-02-11 15:59:21 +0530

    module 13 done

---
+++ S13_Spring_Boot/src/main/java/lk/ijse/s13_spring_boot/S13SpringBootApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.s13_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S13SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S13SpringBootApplication.class, args);
+    }
+
+}
---
+++ S13_Spring_Boot/src/test/java/lk/ijse/s13_spring_boot/S13SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.s13_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S13SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
