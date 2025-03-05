841e9c4059b28efa550443e19a5685c69747ba67
kavindisathsarani <dksmanimendra0@gmail.com>
2025-03-05 11:13:34 +0530

    module 16

--- S16_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/s16_spring_boot_bean_validation/controller/CustomerController.java
+++ S16_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/s16_spring_boot_bean_validation/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package lk.ijse.s16_spring_boot_bean_validation.controller;
 
+import jakarta.validation.Valid;
 import lk.ijse.s16_spring_boot_bean_validation.dto.CustomerDTO;
 import lk.ijse.s16_spring_boot_bean_validation.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
@@ -12,14 +13,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
+    /*send POST request to http://localhost:8080/api/v1/customer/save
+    select body-raw then type this then send a request
+     {
+    "name":"kavi",
+    "email":"kavi@gmail.com",
+    "phoneNumber":"0777876756"
+    }
+
+The below thing wil get printed in the postman.
+
+    {
+    "message": "User Created successfully",
+    "statusCode": 201,
+    "data": {
+        "name": "kavi",
+        "email": "kavi@gmail.com",
+        "phoneNumber": "0777876756"
+    }
+}
+*/
+
+    /*after coding GlobalExceptionHandler class if you send an invalid one the warning will get printed as the blow example.
+
+    send POST request to http://localhost:8080/api/v1/customer/save
+    select body-raw then type this then send a request{
+    "name":"kavi",
+    "email":"kavigmail.com",
+    "phoneNumber":"077787676"
+
+}
+
+ the below will get printed in the postman
+ {
+    "message": "Validation failed",
+    "statusCode": 400,
+    "data": {
+        "phoneNumber": "Phone number must be exactly 10 digits",
+        "email": "Invalid email format"
+    }
+}
+ */
     @PostMapping("/save")
-    public ResponseEntity<ResponseDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
-        ResponseDTO responseDTO=new ResponseDTO(
+    public ResponseEntity<ResponseDTO> createCustomer(@Valid @RequestBody CustomerDTO customerDTO) {
+        ResponseDTO responseDTO = new ResponseDTO(
                 "User Created successfully",
                 HttpStatus.CREATED.value(),
                 customerDTO
         );
-        return new ResponseEntity(responseDTO,HttpStatus.CREATED);
+        return new ResponseEntity(responseDTO, HttpStatus.CREATED);
     }
 
 }
--- S16_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/s16_spring_boot_bean_validation/dto/CustomerDTO.java
+++ S16_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/s16_spring_boot_bean_validation/dto/CustomerDTO.java
@@ -1,9 +1,20 @@
 package lk.ijse.s16_spring_boot_bean_validation.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class CustomerDTO {
+    @NotBlank(message = "Name is required")
+    @Pattern(regexp = "^[A-Za-z]+$", message = "Name should contain only letters and spaces")
+    @Size(min = 2,max=30,message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @Email(message = "Invalid email format")
     private String email;
 
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be exactly 10 digits")
     private String phoneNumber;
 
     public CustomerDTO() {
