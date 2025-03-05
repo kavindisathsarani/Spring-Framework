f7aa95a7aa81a3bbd002f9163c7268d7ba0e26af
kavindisathsarani <dksmanimendra0@gmail.com>
2025-03-05 10:19:58 +0530

    module 16 began

---
+++ S16_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/s16_spring_boot_bean_validation/S16SpringBootBeanValidationApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.s16_spring_boot_bean_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S16SpringBootBeanValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S16SpringBootBeanValidationApplication.class, args);
+    }
+
+}
---
+++ S16_Spring_Boot_Bean_Validation/src/test/java/lk/ijse/s16_spring_boot_bean_validation/S16SpringBootBeanValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.s16_spring_boot_bean_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S16SpringBootBeanValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
